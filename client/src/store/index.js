69a6fb1b3fe1ba8a6dbd6ba1c5e68687ac4e591f
Thomas Harvey <thomasharvey24@gmail.com>
2018-01-10 18:19:32 -0500

    Replacing old state load with new convenience function

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -5,16 +5,14 @@ import Vuex from 'vuex';
 import * as actions from '@/store/actions';
 import mutations from '@/store/mutations';
 import * as getters from '@/store/getters';
-import { getItem } from '@/store/utils';
+import { getState } from '@/store/utils';
 
 Vue.use( Vuex );
 
 const debug = process.env.NODE_ENV !== 'production';
 
-var state = JSON.parse( getItem( '__cointracker__' ) || '{ "coins": { "last_updated": null, "items": {} } }' );
-
 export default new Vuex.Store( {
-    state,
+    'state': getState(),
     actions,
     mutations,
     getters,
