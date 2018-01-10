b020627b82e7ad6d3cb18a2484d566a4c626e088
Thomas Harvey <thomasharvey24@gmail.com>
2018-01-09 22:44:20 -0500

    converted function based class to class expression

--- client/src/api/coinmarketcap.js
+++ client/src/api/coinmarketcap.js
@@ -1,28 +1,56 @@
+'use strict';
+
 import ajax from 'utils';
 
 
-// https://coinmarketcap.com/api/
+// DOCS: https://coinmarketcap.com/api/
+
+
 // Limits:
 // Please limit requests to no more than 10 per minute.
 // Endpoints update every 5 minutes.
 
 
-var CoinMarketCapAPI = function(){
-	var api = {
-		"base": "https://api.coinmarketcap.com/v1/"
+var CoinMarketCapAPI = class extends ajax {
+	constructor() {
+		this.base = 'https://api.coinmarketcap.com/v1'
 	};
-	return api;
-};
 
-CoinMarketCapAPI.prototype.getTickers = function(options){
+	getTickers( options ) {
+		/* Optional parameters:
+		 * (int) `start` - return results from rank [start] and above
+		 * (int) `limit` - return a maximum of [limit] results (default is 100, use 0 to return all results)
+		 * (string) `convert` - return price, 24h volume, and market cap in terms of another currency. 
+		 * 	 Valid values are: 
+		 *   	"AUD", "BRL", "CAD", "CHF", "CLP", "CNY", "CZK", 
+		 *   	"DKK", "EUR", "GBP", "HKD", "HUF", "IDR", "ILS", 
+		 *   	"INR", "JPY", "KRW", "MXN", "MYR", "NOK", "NZD", 
+		 *   	"PHP", "PKR", "PLN", "RUB", "SEK", "SGD", "THB", 
+		 *   	"TRY", "TWD", "ZAR"
+		 */
+		return this.get( `${this.base}/ticker/`, options || {} );
+	};
 
-};
+	getTicker( options ) {
+		/* Optional parameters:
+		 * (string) `convert` - return price, 24h volume, and market cap in terms of another currency. 
+		 * 	Valid values are: 
+		 *  	"AUD", "BRL", "CAD", "CHF", "CLP", "CNY", "CZK", "DKK", "EUR", "GBP", 
+		 *  	"HKD", "HUF", "IDR", "ILS", "INR", "JPY", "KRW", "MXN", "MYR", "NOK", 
+		 *  	"NZD", "PHP", "PKR", "PLN", "RUB", "SEK", "SGD", "THB", "TRY", "TWD", "ZAR"
+		 */
+		return this.get( `${this.base}/ticker/${options.id}/`, options || {} );
+	};
 
-
-CoinMarketCapAPI.prototype.getTicker = function(options){
-
-};
-
-CoinMarketCapAPI.prototype.getGlobal = function(options){
-
+	getGlobal( options ) {
+		/* Optional parameters:
+		 * (string) `convert` - return 24h volume, and market cap in terms of another currency. 
+		 *  Valid values are: 
+		 *		"AUD", "BRL", "CAD", "CHF", "CLP", "CNY", "CZK", "DKK", "EUR", 
+		 *		"GBP", "HKD", "HUF", "IDR", "ILS", "INR", "JPY", "KRW", "MXN", 
+		 *		"MYR", "NOK", "NZD", "PHP", "PKR", "PLN", "RUB", "SEK", "SGD", 
+		 *		"THB", "TRY", "TWD", "ZAR"
+		 */
+		return this.get( `${this.base}/global/`,  options || {} );
+	};
 };
---
+++ client/src/api/utils.js
@@ -0,0 +1,8 @@
+'use strict';
+
+import axios from 'axios';
+
+
+export default {
+	"ajax": axios
+};
